1c20c48b444bdd552cca6309705de55228b96ea5
Geoffrey Roberts <groberts@anchor.net.au>
2014-12-19 16:10:01 +1100

    Added datetime parser and test

--- lib/Netsuite/Parsers.hs
+++ lib/Netsuite/Parsers.hs
@@ -22,6 +22,7 @@ import qualified Data.Text                         as ST
 import qualified Data.Text.Lazy                    as LT
 import qualified Data.Text.Lazy.Builder            as LT
 import qualified Data.Text.Lazy.Builder.Scientific as LT
+import           Data.Time
 import           Text.Read                         (readMaybe)
 
 -- | Parse a netsuite currency field, which is a blob of text looking like:
@@ -37,3 +38,13 @@ currency = demote (prism' f g) <$> value
     -- We render with arbitrary precision (None) with standard decimal notation
     -- (Fixed)
     fmt = LT.formatScientificBuilder Fixed Nothing
+
+-- | Parse a netsuite datetime field, which is looks like:
+--
+--    "20/6/2014 4:25 pm"
+datetime :: JsonSyntax s => s UTCTime
+datetime = demote (prism' f g) <$> value
+  where
+    f = String . LT.toStrict . LT.toLazyText . LT.fromText . ST.pack . formatTime defaultTimeLocale fmt
+    g = parseTimeM True defaultTimeLocale fmt . ST.unpack <=< preview _String
+    fmt = "%d/%m/%Y %l:%M %P"
